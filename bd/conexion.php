9a751d6c2798f112b8710967ee6aafacc418c954
Esteban-MTAS <20241860@itla.edu.do>
2026-04-06 22:59:11 -0400

    Verificacion de DB

--- bd/conexion.php
+++ bd/conexion.php
@@ -1,11 +1,11 @@
 <?php
-$host = "localhost";
-$dbname = "dblibreria";
-$user = "root";
-$pass = "";
+$host = "sql202.infinityfree.com";
+$dbname = "if0_41597473_biblioteca";
+$user = "if0_41597473";
+$pass = "Acosta2004456";
 
 try {
-    $conexion = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $conexion = new PDO("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8", $user, $pass);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
